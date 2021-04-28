1aecddfc459f2b71041f7595f87716eb90f63df7
tolgacanelmas <e.tolgacan@gmail.com>
2021-04-29 01:51:55 +0300

    localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,30 @@
+
+
 $(document).ready(function () {
-  const movies = JSON.parse(localStorage.getItem("json"));
-  movies.forEach((movie, i) => {
-    const singleMovie = `
-                <div class="movie"> 
-                    <div>${movie.name}</div>
-                    <div>${movie.point}</div>
-                    <div>
-                        <span>Puan Ver</span>
-                        <span class="material-icons-outlined"> arrow_drop_up </span>
-                        <span class="material-icons-outlined"> arrow_drop_down </span>
-                    </div>
-                </div>
-            `;
-    $(".movies").append(singleMovie);
-  });
+  
+  $.getJSON('../assets/movies.json', function (movies) {
+    movies.forEach((movie, i) => {
+      const singleMovie = `
+                  <div class="movie"> 
+                      <div class="movie-name">${movie.name}</div>
+                      <div class="movie-point">
+                      <span class="material-icons-outlined">
+                      star
+                      </span>
+                      ${movie.point}</div>
+                      <div class="give-point">
+                          <span>Puan Ver</span>
+                          <span class="material-icons-outlined"> arrow_drop_up </span>
+                          <span class="material-icons-outlined"> arrow_drop_down </span>
+                      </div>
+                  </div>
+              `;
+      $(".movies").append(singleMovie);
+    });
+  })
 
   //menu buttons
   $(".left div button").on("click", function () {
     $(this).next().toggleClass("active");
-  });
-
-  // menu sub buttons
-
-  let nameOfMovieOrShow = "";
-  let point_ = "";
-  let category = "";
-
-  $("#name").on("keyup", function () {
-    nameOfMovieOrShow = $(this).val();
-  });
-
-  $(".point").on("keyup", function () {
-    point_ = $(this).val();
-  });
-
-  $(".select-type")
-    .children("div")
-    .each(function () {
-      $(this)
-        .find("input")
-        .on("change", function () {
-          category = $(this).val();
-        });
-    });
-
-  $(".save-new").on("click", function () {
-    const newTypeObject = {
-      name: nameOfMovieOrShow,
-      point: point_,
-      kind: category,
-    };
-    $.getJSON("../assets/movies.json", function (movies) {
-      movies.push(newTypeObject);
-      localStorage.setItem("json", JSON.stringify(movies));
-    });
-
-    Swal.fire({
-      title: "Başarılı bir şekilde kaydedildi!",
-      icon: "success",
-      confirmButtonText: "Devam",
-    });
-
-    $('.new-sign-container').find('input').each(function(){
-        $(this).val('');
-    })
   });
 });
